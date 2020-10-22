1a27067c5adff3c65ad98c5c9d77cf865b0c9278
xiaohongyao <xiaohongyao@meituan.com>
2020-10-22 14:51:32 +0800

    Initial Commit

---
+++ oc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  oc
+//
+//  Created by xiaohongyao on 22/10/2020.
+//  Copyright © 2020 xiaohongyao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
